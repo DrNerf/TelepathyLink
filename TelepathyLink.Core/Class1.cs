34b412f433f1b98f7248524cc51b07e12bc159ec
Svetoslav Todorov <drnerf95@gmail.com>
2019-02-26 00:29:58 +0200

    Project setup.

--- TelepathyLink.Core/Class1.cs
+++ TelepathyLink.Core/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using Telepathy;
 
 namespace TelepathyLink.Core
 {
     public class Class1
     {
+        public Class1()
+        {
+            var server = new Server();
+            server.Start(8080);
+        }
     }
 }
